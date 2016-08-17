9e54dbc983c6788462bea2a7fa67e09ba93ff10c
fabian.gaebel <gaebel@csgaebel.de>
2016-08-17 21:14:51 +0200

    remove *.class files

--- src/main/java/de/csgaebel/gohome/repository/WorkdaysRepository.java
+++ src/main/java/de/csgaebel/gohome/repository/WorkdaysRepository.java
@@ -6,4 +6,16 @@ import de.csgaebel.gohome.model.*;
 
 public class WorkdaysRepository {
 	ArrayList<Workday> workdays = new ArrayList<Workday>();
+	
+	public void setWorkdays(ArrayList<Workday> workdays) {
+		this.workdays = workdays;
+	}
+	
+	public ArrayList<Workday> getWorkdays() {
+		return workdays;
+	}
+	
+	public void addWorkday(Workday workday){
+		
+	}
 }
